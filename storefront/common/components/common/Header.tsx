1fbeeccfd5860a11776d208246fc3c9da5b0d69e
23120337 <bmquan.GD@gmail.com>
2026-07-05 16:05:04 +0700

    style(storefront): change header title text and apply inline red color

--- storefront/common/components/common/Header.tsx
+++ storefront/common/components/common/Header.tsx
@@ -121,7 +121,7 @@ const Header = ({ children }: Props) => {
         <nav className="limiter-menu-desktop container">
           {/* <!-- Logo desktop --> */}
           <Link href="/" className="header-logo me-3">
-            <h3 className="text-danger">Yas - Storefront CI/CD OK (MOdify)</h3>
+            <h3 style={{ color: 'red' }}>Yas - Storefront CI/CD RED (MOdify)</h3>
           </Link>
 
           {/* <!-- Search --> */}
